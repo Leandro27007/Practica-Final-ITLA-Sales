4b90413e2d12de1da5ac56802517aea471a5c1f2
Leandro27007 <125936141+Leandro27007@users.noreply.github.com>
2024-03-26 23:55:59 -0400

    Negocio Service, Venta Service Ready 25%

---
+++ Sales.AppServices/Services/NegocioService.cs
@@ -0,0 +1,49 @@
+﻿using Sales.AppServices.Contracts;
+using Sales.AppServices.Core;
+using Sales.AppServices.Dtos;
+using Sales.Domain.Entities;
+using Sales.Infraestructure.Interfaces;
+
+namespace Sales.AppServices.Services
+{
+    public class NegocioService : INegocioService
+    {
+        private INegocioDb _negocioDb;
+        public NegocioService(INegocioDb negocioDb)
+        {
+            this._negocioDb = negocioDb;
+        }
+        public async Task<ServiceResult> AddNegocio(NegocioAddDTO negocioAdd)
+        {
+            ServiceResult result = new();
+
+            Negocio negocio = new()
+            {
+                Correo = negocioAdd.Correo,
+                Direccion = negocioAdd.Direccion,
+                Nombre = negocioAdd.Nombre,
+                UrlLogo = negocioAdd.UrlLogo,
+                Telefono = negocioAdd.Telefono,
+                SimboloMoneda = negocioAdd.SimboloMoneda,
+                PorcentajeImpuesto = negocioAdd.PorcentajeImpuesto,
+                NumeroDocumento = negocioAdd.NumeroDocumento,
+                NombreLogo = negocioAdd.NombreLogo
+
+            };
+
+            var dataResult = await _negocioDb.Save(negocio);
+
+
+            result.Success = dataResult.Success;
+            result.Message = dataResult.Message;
+
+
+            return result;
+        }
+
+        public Task<ServiceResult> GetNegocio()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sales.AppServices/Services/VentaService.cs
@@ -0,0 +1,19 @@
+﻿using Sales.AppServices.Contracts;
+using Sales.AppServices.Core;
+using Sales.AppServices.Dtos;
+
+namespace Sales.AppServices.Services
+{
+    public class VentaService : IVentaService
+    {
+        public Task<ServiceResult> GetVenta(int numeroVenta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResult> HacerVenta(HacerVentaDTO venta)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
